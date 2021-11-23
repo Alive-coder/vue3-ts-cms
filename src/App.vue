3a3b8f3dfca0106c3ab35d00a6455886b11cabf9
Alive-coder <1850290043@qq.com>
2021-11-23 12:39:50 +0800

    fix: fsds
    dsf

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  你好世界
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
     HelloWorld
   }
-});
+})
 </script>
 
 <style lang="less">
